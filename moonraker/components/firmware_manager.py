74dfd37380f0e6a9bfd25cf29a8d13585e2082a1
ruipeng <1041589370@qq.com>
2025-11-08 09:26:48 +0800

    适配D1000Pro机型的下位机更新，并优化了更新通知

--- moonraker/components/firmware_manager.py
+++ moonraker/components/firmware_manager.py
@@ -9,17 +9,45 @@ from .klippy_apis import KlippyAPI
 from typing import (
     TYPE_CHECKING,
     Any,
+    Union,
     Dict,
 )
 
 if TYPE_CHECKING:
     from .machine import Machine
-    from .klippy_connection import KlippyConnection as Klippy
 
 HOME = os.path.expanduser("~")
 KLIPPER_DIR = os.path.join(HOME, "klipper/firmware")
 MAIN_DEV = "/dev/ttyACM0"
 SYSTEM_PYTHON = "python3"
+
+class FirmwareUpdateNotifier:
+    def __init__(self, server) -> None:
+        self.server = server
+        self.full_complete = False
+        self.firmware = ""
+    def _set_current_firmware(self, firmware: str) -> None:
+        self.firmware = firmware
+    def _set_full_complete(self, complete: bool) -> None:
+        self.full_complete = complete
+        if complete:
+            self.notify_update_response(f"All firmware update finished!", is_complete=complete)
+        elif not complete:
+            self.notify_update_response(f"Firmware update failed!", is_complete=True)
+    def notify_update_response(
+            self, resp: Union[str, bytes], is_complete: bool = False
+        ) -> None:
+            resp = resp.strip()
+            if isinstance(resp, bytes):
+                resp = resp.decode()
+            done = is_complete
+            done &= self.full_complete
+            notification = {
+                'message': resp,
+                'application': self.firmware,
+                'complete': done}
+            self.server.send_event(
+                "update_manager:update_response", notification)
 
 class FirmwareUpdate:
     def __init__(self, config: ConfigHelper) -> None:
@@ -31,6 +59,7 @@ class FirmwareUpdate:
         self.current_progress = 0
         self.updating = False
         self.need_check_update = True
+        self.notifier = None
         self.server.register_event_handler(
             "server:klippy_started", self._on_klippy_startup)
 
@@ -42,11 +71,23 @@ class FirmwareUpdate:
         return self.updating
 
     async def _on_klippy_startup(self, state: KlippyState) -> None:
-        if not self.need_check_update or not self._is_firmware_dir_exists():
+        if not self.need_check_update or not self._check_firmware_exists():
             return
+        self.notifier = FirmwareUpdateNotifier(self.server)
         try:
             await self.build_mcu_info()
-            logging.info(f"{self.mcu_info}")
+            table = []
+            for device, info in self.mcu_info.items():
+                table.append(
+                    f"{device:<12} "
+                    f"{info['canbus_uuid']:<14} "
+                    f"{info.get('mcu_version', 'Unknown'):<9} "
+                    f"{info['mcu_type']:<14} "
+                    f"{'Yes' if info['need_update'] else 'No':<12}"
+                )
+            header = f"{'Device':<12} {'CANbus UUID':<14} {'Version':<9} {'MCU Type':<14} {'Update':<7}"
+            separator = "-" * len(header)
+            logging.info("MCU Information:\n" + "\n".join([header, separator] + table))
         except Exception as e:
             logging.exception(f"An error occurred during the building mcu info process: {e}")
         for mcu_data in self.mcu_info.values():
@@ -62,18 +103,28 @@ class FirmwareUpdate:
         except self.server.error:
             pass
 
-    def _is_firmware_dir_exists(self):
-        return os.path.exists(KLIPPER_DIR)
+    def _check_firmware_exists(self, firmware_path: str = None) -> bool:
+        if not os.path.exists(KLIPPER_DIR):
+            return False
+        if firmware_path:
+            return os.path.exists(firmware_path)
+        return True
 
     async def start_update(self):
         try:
             self.updating = True
+            uinfo: Dict[str, Any] = {}
+            uinfo['busy'] = True
+            uinfo['application'] = "firmware"
+            self.server.send_event("update_manager:update_refreshed", uinfo)
             await self._do_klipper_action("stop")
             await self.upgrade_needed_tool_mcus()
             await self.upgrade_mcu()
+            self.notifier._set_full_complete(True)
             await self._do_klipper_action("start")
             self.updating = False
         except Exception as e:
+            self.notifier._set_full_complete(False)
             logging.exception(f"An error occurred during the update process: {e}")
         self.updating = False
 
@@ -105,15 +156,19 @@ class FirmwareUpdate:
             try:
                 response = await self.klippy_apis.query_objects({mcu: None})
                 mcu_data = response.get(mcu, {})
-                mcu_version: str = mcu_data.get('mcu_version', '')
-                if mcu == "mcu":
-                    self.min_version = mcu_data.get('min_firmware_version', "")
-                if mcu_version:
-                    self.need_check_update = False
-                    short_version: str = mcu_version.split('-')[0]
-                    if short_version.lower().startswith('v'):
-                        short_version = short_version[1:]
-                    self.mcu_info[mcu]['mcu_version'] = short_version
+                if mcu_data:
+                    mcu_version: str = mcu_data.get('mcu_version', '')
+                    mcu_constants: str = mcu_data.get('mcu_constants', '')
+                    mcu_type: str = mcu_constants.get('MCU', '')
+                    if mcu == "mcu":
+                        self.min_version = mcu_data.get('min_firmware_version', "")
+                    if mcu_version and mcu_type:
+                        self.need_check_update = False
+                        short_version: str = mcu_version.split('-')[0]
+                        if short_version.lower().startswith('v'):
+                            short_version = short_version[1:]
+                        self.mcu_info[mcu]['mcu_version'] = short_version
+                        self.mcu_info[mcu]['mcu_type'] = mcu_type
             except Exception as e:
                 logging.error(f"Error querying {mcu}: {e}")
 
@@ -153,59 +208,87 @@ class FirmwareUpdate:
             else:
                 self.mcu_info[mcu]['need_update'] = False
 
-    async def upgrade_needed_tool_mcus(self):
-        firmware_mapping = {
-            "mcu L_tool": os.path.join(KLIPPER_DIR, "F072_L.bin"),
-            "mcu R_tool": os.path.join(KLIPPER_DIR, "F072_R.bin"),
-            "mcu tool": os.path.join(KLIPPER_DIR, "F072_L.bin")
+    def _get_firmware_path(self, mcu_name, mcu_type):
+        STANDARD_MCU_FIRMWARE = {
+            "mcu L_tool": "F072_L.bin",
+            "mcu R_tool": "F072_R.bin",
+            "mcu detect": "detect.bin",
+            "mcu motion": "motion.bin",
+            "mcu AC": "AC.bin",
         }
+        if mcu_name in STANDARD_MCU_FIRMWARE:
+            firmware_file = STANDARD_MCU_FIRMWARE[mcu_name]
+            return os.path.join(KLIPPER_DIR, firmware_file)
+        if mcu_name == "mcu tool":
+            TOOL_MCU_FIRMWARE = {
+                "stm32f072xb": "F072_L.bin",
+                "stm32g431xx": "G431_L.bin"
+            }
+            if mcu_type in TOOL_MCU_FIRMWARE:
+                firmware_file = TOOL_MCU_FIRMWARE[mcu_type]
+                return os.path.join(KLIPPER_DIR, firmware_file)
+            else:
+                raise ValueError(f"Unsupported MCU type '{mcu_type}' for 'mcu tool'")
+
+    async def upgrade_needed_tool_mcus(self):
         for mcu_name, mcu_data in self.mcu_info.items():
-            if mcu_data.get('need_update', False) and mcu_name != "mcu":
-                firmware_path = firmware_mapping.get(mcu_name)
-                if not firmware_path:
+            if mcu_data.get('need_update', False) and mcu_name is not "mcu":
+                firmware_path = self._get_firmware_path(mcu_name, mcu_data.get('mcu_type', ''))
+                if firmware_path and self._check_firmware_exists(firmware_path):
+                    self.notifier._set_current_firmware(mcu_name)
+                    try:
+                        flash_tool = FlashTool(
+                            self.notifier,
+                            uuid = mcu_data['canbus_uuid'],
+                            firmware = firmware_path
+                        )
+                        exit_code = await flash_tool.run()
+                        await asyncio.sleep(0.5)
+                        if exit_code == 0:
+                            logging.info(f"Upgrade operation for {mcu_name} succeeded!")
+                        else:
+                            logging.error(f"Upgrade operation for {mcu_name} failed, exit code: {exit_code}")
+                    except Exception as e:
+                        logging.exception(f"An exception occurred during the upgrade process for {mcu_name}: {e}")
+                else:
                     logging.warning(f"No firmware specified for {mcu_name}, skipping upgrade.")
-                    continue
-                try:
-                    flash_tool = FlashTool(
-                        self.server,
-                        uuid = mcu_data['canbus_uuid'],
-                        firmware = firmware_path
-                    )
-                    exit_code = await flash_tool.run()
-                    await asyncio.sleep(1)
-                    if exit_code == 0:
-                        logging.info(f"Upgrade operation for {mcu_name} succeeded, exit code: {exit_code}")
-                    else:
-                        logging.error(f"Upgrade operation for {mcu_name} failed, exit code: {exit_code}")
-                except Exception as e:
-                    logging.exception(f"An exception occurred during the upgrade process for {mcu_name}: {e}")
 
     async def upgrade_mcu(self):
+        MCU_FIRMWARE_MAP = {
+                "stm32f446xx": "F446.bin",
+                "stm32g0b1xx": "G0B1.bin"
+            }
         for mcu_name, mcu_data in self.mcu_info.items():
             if mcu_name == "mcu" and mcu_data.get('need_update', False):
-                try:
-                    flash_tool = FlashTool(
-                        self.server,
-                        uuid=mcu_data['canbus_uuid'],
-                        request_bootloader=True
-                    )
-                    exit_code = await flash_tool.run()
-                    if exit_code != 0:
-                        logging.error(f"Failed to request bootloader for {mcu_name}, exit code: {exit_code}")
-                        continue
-                    await asyncio.sleep(2)
-                    flash_tool = FlashTool(
-                        self.server,
-                        device=MAIN_DEV,
-                        firmware=os.path.join(KLIPPER_DIR, "F446.bin")
-                    )
-                    exit_code = await flash_tool.run()
-                    if exit_code == 0:
-                        logging.info(f"Upgrade operation for {mcu_name} succeeded, exit code: {exit_code}")
-                    else:
-                        logging.error(f"Upgrade operation for {mcu_name} failed, exit code: {exit_code}")
-                except Exception as e:
-                    logging.exception(f"An exception occurred during the upgrade process for {mcu_name}: {e}")
+                firmware_name = MCU_FIRMWARE_MAP[mcu_data.get('mcu_type', '')]
+                firmware_path = os.path.join(KLIPPER_DIR, firmware_name)
+                if firmware_name and self._check_firmware_exists(firmware_path):
+                    self.notifier._set_current_firmware(mcu_name)
+                    try:
+                        flash_tool = FlashTool(
+                            self.notifier,
+                            uuid=mcu_data['canbus_uuid'],
+                            request_bootloader=True
+                        )
+                        exit_code = await flash_tool.run()
+                        if exit_code != 0:
+                            logging.error(f"Failed to request bootloader for {mcu_name}, exit code: {exit_code}")
+                            continue
+                        await asyncio.sleep(0.5)
+                        flash_tool = FlashTool(
+                            self.notifier,
+                            device=MAIN_DEV,
+                            firmware=os.path.join(KLIPPER_DIR, firmware_file)
+                        )
+                        exit_code = await flash_tool.run()
+                        if exit_code == 0:
+                            logging.info(f"Upgrade operation for {mcu_name} succeeded, exit code: {exit_code}")
+                        else:
+                            logging.error(f"Upgrade operation for {mcu_name} failed, exit code: {exit_code}")
+                    except Exception as e:
+                        logging.exception(f"An exception occurred during the upgrade process for {mcu_name}: {e}")
+                else:
+                    logging.warning(f"No firmware specified for {mcu_name}, skipping upgrade.")
 
 def load_component(config: ConfigHelper) -> FirmwareUpdate:
     return FirmwareUpdate(config)
